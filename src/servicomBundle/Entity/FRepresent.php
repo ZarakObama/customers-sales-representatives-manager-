031b6ef4d525c8f862807975de25ae833f3901d1
ZarakObama <malek.zarkouna@esprit.tn>
2017-08-04 16:33:48 +0100

    register commercial

--- src/servicomBundle/Entity/FRepresent.php
+++ src/servicomBundle/Entity/FRepresent.php
@@ -21,49 +21,49 @@ class FRepresent extends BaseUser
      *
      * @ORM\Column(name="RE_No", type="integer", nullable=true)
      */
-    private $reNo;
+    public $reNo;
 
     /**
      * @var string
      *
      * @ORM\Column(name="RE_Nom", type="string", length=12, nullable=true)
      */
-    private $reNom;
+    public $reNom;
 
     /**
      * @var string
      *
      * @ORM\Column(name="RE_Prenom", type="string", length=9, nullable=true)
      */
-    private $rePrenom;
+    public $rePrenom;
 
     /**
      * @var string
      *
      * @ORM\Column(name="RE_Region", type="string", length=9, nullable=true)
      */
-    private $reRegion;
+    public $reRegion;
 
     /**
      * @var string
      *
      * @ORM\Column(name="RE_Telephone", type="string", length=5, nullable=true)
      */
-    private $reTelephone;
+    public $reTelephone;
 
     /**
      * @var string
      *
      * @ORM\Column(name="RE_EMail", type="string", length=10, nullable=true)
      */
-    private $reEmail;
+    public $reEmail;
 
     /**
      * @var string
      *
      * @ORM\Column(name="RE_DateCreation", type="string", length=19, nullable=true)
      */
-    private $reDatecreation;
+    public $reDatecreation;
 
     /**
      * @var integer
@@ -72,7 +72,7 @@ class FRepresent extends BaseUser
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
-    protected $id;
+    public $id;
 
 
     /**
@@ -81,19 +81,19 @@ class FRepresent extends BaseUser
      * @Vich\UploadableField(mapping="product_image", fileNameProperty="imageName")
      *
      */
-    private $imageFile;
+    public $imageFile;
     /**
      * @ORM\Column(type="string", length=255,nullable=true)
      *
      * @var string
      */
-    private $imageName;
+    public $imageName;
     /**
      * @ORM\Column(type="datetime",nullable=true)
      *
      * @var \DateTime
      */
-    private $updatedAt;
+    public $updatedAt;
     /**
      * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
      * of 'UploadedFile' is injected into this setter to trigger the  update. If this
